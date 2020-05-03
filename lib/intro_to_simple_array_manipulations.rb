348a84bb96d4538a0c78411635516dae310ddd71
Spencer Smith <smithspencer817@gmail.com>
2020-05-03 17:29:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,11 +11,13 @@ def using_uniq(arr)
 end
 
 def using_flatten(arr)
-  
+  arr.flatten
 end
 
 def using_delete(arr,str)
+  arr.delete(str)
 end
 
 def using_delete_at(arr,int)
+  arr.delete_at(int)
 end
